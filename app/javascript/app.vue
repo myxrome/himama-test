85b304aacc9af2a13ba3a0e09ec44d25aa94cede
Aleksey Zolotov <myxrome@gmail.com>
2020-01-24 17:55:38 +0400

    - added styles for home page.

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,37 +1,42 @@
 <template>
   <div>
-    <div v-if="current_clock_log">
-      <p>Clocked in at {{ current_clock_log.clocked_in_at }}</p>
-      <button v-on:click="clockOut">Clock Out</button>
+    <div class="text-center" v-if="current_clock_log">
+      <h3>You are clocked in at {{ current_clock_log.clocked_in_at }}</h3>
+      <button type="button" class="button radius bordered shadow primary" v-on:click="clockOut">Clock Out</button>
     </div>
-    <div v-else>
-      <p>You haven't been clocked in yet</p>
-      <button v-on:click="clockIn">Clock In</button>
+    <div class="text-center" v-else>
+      <h3>You haven't been clocked in yet</h3>
+      <button type="button" class="button radius bordered shadow primary" v-on:click="clockIn">Clock In</button>
     </div>
-    <table>
+    <hr>
+    <h3 class="text-center">Clock Log History</h3>
+    <table class="radius bordered shadow">
       <thead>
-        <tr>
-          <th>Clock In</th>
-          <th>Clock Out</th>
-          <th></th>
-          <th></th>
-        </tr>
+      <tr class="text-center">
+        <th class="text-center">#</th>
+        <th class="text-center">Clock In</th>
+        <th class="text-center">Clock Out</th>
+        <th></th>
+        <th></th>
+      </tr>
       </thead>
       <tbody>
-        <tr v-for="clock_log in completed_clock_logs">
-          <template v-if="clock_log === editable_clock_log">
-            <td><input v-model="clock_log.clocked_in_at"></td>
-            <td><input v-model="clock_log.clocked_out_at"></td>
-            <td><button v-on:click="save(clock_log)">Save</button></td>
-            <td><button v-on:click="cancelEdit">Cancel</button></td>
-          </template>
-          <template v-else>
-            <td>{{ clock_log.clocked_in_at }}</td>
-            <td>{{ clock_log.clocked_out_at }}</td>
-            <td><button v-on:click="editClockLog(clock_log)">Edit</button></td>
-            <td><button v-on:click="destroy(clock_log)">Delete</button></td>
-          </template>
-        </tr>
+      <tr class="text-center" v-for="(clock_log, index) in completed_clock_logs">
+        <template v-if="clock_log === editable_clock_log">
+          <td>{{ index + 1 }}</td>
+          <td><input v-model="clock_log.clocked_in_at"></td>
+          <td><input v-model="clock_log.clocked_out_at"></td>
+          <td><button type="button" class="button radius bordered shadow primary" v-on:click="save(clock_log)">Save</button></td>
+          <td><button type="button" class="button radius bordered shadow primary" v-on:click="cancelEdit">Cancel</button></td>
+        </template>
+        <template v-else>
+          <td>{{ index + 1 }}</td>
+          <td>{{ clock_log.clocked_in_at }}</td>
+          <td>{{ clock_log.clocked_out_at }}</td>
+          <td><button type="button" class="button radius bordered shadow primary" v-on:click="editClockLog(clock_log)">Edit</button></td>
+          <td ><button type="button" class="button radius bordered shadow primary" v-on:click="destroy(clock_log)">Delete</button></td>
+        </template>
+      </tr>
       </tbody>
     </table>
   </div>
@@ -108,6 +113,11 @@ export default {
     refresh: function () {
       axios.get('/clock_logs.json').then((response) => {
         this.clock_logs = response.data
+        this.clock_logs.sort(function (left, right) {
+          if (left.clocked_in_at < right.clocked_in_at) { return -1 }
+          if (left.clocked_in_at > right.clocked_in_at) { return 1 }
+          return 0;
+        })
       }).catch((response) => {
         console.log(response)
       })
